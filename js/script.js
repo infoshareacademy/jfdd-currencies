2625179012bc13963a7bc206efc01d4d5313dfe8
tomasznastaly <nastalytomasz@gmail.com>
2015-12-09 17:12:29 +0100

    Merge branch 'master' of https://github.com/infoshareacademy/jfdd-currencies
    Conflicts:
    	css/style.css
    	index.html

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+    $(window).on("scroll", function () {
+        if($(window).scrollTop() > 50) {
+            $("a").addClass("active-a");
+        } else {
+            $("a").removeClass("active-a");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $(window).on("scroll", function () {
+        if($(window).scrollTop() > 50) {
+            $("#hero_picture").addClass("hero-brightness");
+        } else {
+            $("#hero_picture").removeClass("hero-brightness");
+        }
+    });
+});
+
