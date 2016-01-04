204d07cb1eaa34d16d377df6f6bdf1ac0b4c86a9
ŁukaszKomajda <l.komajda@wp.pl>
2016-01-04 11:31:19 +0100

    poprawki w walidacji formularza

--- js/walidacja_formularza.js
+++ js/walidacja_formularza.js
@@ -3,10 +3,10 @@ function checkForm() {
     var timeChangeToSeconds = 1000;
     var time = date.getTime() / timeChangeToSeconds;
     var newDate = new Date();
-    var newTime =newDate.getTime() / timeChangeToSeconds;
-    var checkTime = newTime- time ;
+    var newTime = newDate.getTime() / timeChangeToSeconds;
+    var checkTime = newTime - time;
 
-    if (checkTime>2) {
+    if (checkTime > 2) {
         errorMail = '';
         errorNumber = '';
         testMail();
@@ -19,39 +19,36 @@ function checkForm() {
             mailComment.innerHTML = '';
             sendForm.submit();
         }
-        else{
+        else {
             date = new Date();
         }
 
-        if (errorNumber) {
-            numberComment = document.getElementById('divNumberErrorMassage');
-            numberComment.innerHTML = errorNumber;
-        }
-        else {
-            numberComment = document.getElementById('divNumberErrorMassage');
-            numberComment.innerHTML = '';
-        }
-
-        if (errorMail) {
-            mailComment = document.getElementById('divMailErrorMassage');
-            mailComment.innerHTML = errorMail;
-        }
-        else {
-            mailComment = document.getElementById('divMailErrorMassage');
-           mailComment.innerHTML = '';
-        }
+        displayError(errorNumber, 'divNumberErrorMassage');
+        displayError(errorMail, 'divMailErrorMassage');
 
     }
-    else{
+    else {
         date = new Date();
     }
 }
 
-function testMail(){
+function displayError(errorIndicator, errorMessageDivId) {
+    if (errorIndicator) {
+        numberComment = document.getElementById(errorMessageDivId);
+        numberComment.innerHTML = errorIndicator;
+    }
+    else {
+        numberComment = document.getElementById(errorMessageDivId);
+        numberComment.innerHTML = '';
+    }
+
+}
+
+function testMail() {
 
     var mailValue = document.getElementById('email').value,
-    mailCharacters = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/,
-    checkMail = mailCharacters.test(mailValue);
+        mailCharacters = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/,
+        checkMail = mailCharacters.test(mailValue);
 
     if (mailValue == '') {
         errorMail = 'Pole nie może być puste';
@@ -63,9 +60,9 @@ function testMail(){
     }
 }
 
-function testNumber (){
+function testNumber() {
     var numberValue = document.getElementById('numer').value;
-    number = numberValue.replace(new RegExp(" ","g"),"");
+    number = numberValue.replace(new RegExp(" ", "g"), "");
     var numberCharacters = /^[+]{0,1}[1-9]{0,2}[1-9]{1}[0-9]{2}[-]{0,1}[0-9]{3}[-]{0,1}[0-9]{3}$/,
         homeNumberCharacters = /^[0+]{0,1}[1-9]{0,2}[1-9]{0,2}[1-9]{1}[0-9-]{6,8}$/,
         premiumRateNumberCharacters = /^[7]{1}[0-1]{1}[0-9]{1}[0-9-]{6,8}$/,
