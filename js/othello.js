f29197f8b128354d287025adef7a644c237d1cbe
ŁukaszKomajda <l.komajda@wp.pl>
2016-01-07 13:45:55 +0100

    system przejmowania monet

--- js/othello.js
+++ js/othello.js
@@ -1,7 +1,29 @@
-for (var x = 0; x < 8; ++x) {
-    document.write('<tr>');
-    for (var y = 0; y < 8; ++y) {
-        document.write('<td class=othelloSquare id=r' + x + y + '>' + '</td>');
+
+var typeTd = 'czarny' ;
+
+$('.othelloSquare').click(function(){
+    if(!$(this).hasClass('czarny') && !$(this).hasClass('bialy')) {
+        if (typeTd == 'czarny') {
+            $(this).html('<img src="image/zlotaMoneta.jpg" class="monetaGry">');
+            typeTd = 'bialy';
+            $(this).addClass('czarny');
+            takeCoin($(this));
+        }
+        else {
+            $(this).html('<img src="image/srebrnaMoneta.jpg" class="monetaGry">');
+            typeTd = 'czarny';
+            $(this).addClass('bialy');
+            takeCoin($(this));
+        }
     }
-    document.write('</tr>');
+
+});
+function takeCoin (ClickedTd) {
+    idClickedTd =ClickedTd.attr('id');
+    $('#raport').html(idClickedTd);
+    checkHorizontal=idClickedTd.charAt(1);
+    checkVertical=idClickedTd.charAt(0);
+    if(){
+
+    }
 }
