0a163642776fecc706b5f7c59df0b3bb8a9eebe8
lukaszKomajda <l.komajda@wp.pl>
2016-01-07 23:48:32 +0100

    othello funkcja czy mozna postawic monete

--- js/othello.js
+++ js/othello.js
@@ -1,54 +1,80 @@
 var typeTd = 'czarny';
+arrayListWhite =[];
+arrayListBlack = [];
 
 $('.othelloSquare').click(function () {
     if (!$(this).hasClass('czarny') && !$(this).hasClass('bialy')) {
         if (typeTd == 'czarny') {
-            $(this).html('<img src="image/zlotaMoneta.jpg" class="monetaGry">');
+            imgWhite = '<img src="image/zlotaMoneta.png" class="monetaGry">';
+
             typeTd = 'bialy';
-            $(this).addClass('czarny');
-            takeCoin($(this), 'bialy');
+            pickCoin($(this), imgWhite, typeTd);
+            arrayListWhite.push($(this).attr('id'));
+           // for (i = 0; i < arrayListWhite.length; i++) {
+
+          //  }
+
         }
         else {
-            $(this).html('<img src="image/srebrnaMoneta.jpg" class="monetaGry">');
+            imgBlack = ' <img src="image/srebrnaMoneta.png" class="monetaGry">';
             typeTd = 'czarny';
-            $(this).addClass('bialy');
-            takeCoin($(this), 'czarny');
+            pickCoin($(this), imgBlack, typeTd);
+            arrayListBlack.push($(this).attr('id'));
         }
     }
 
 });
-function takeCoin(ClickedTd) {
+function pickCoin(ClickedTd, imgCoin, typeClass) {
     idClickedTd = ClickedTd.attr('id');
-    //for sprawdzajacy czy moge psotawic monete(wszystkie pola dookola)
-    //sprawdzanie liniowo
-    //sprawdzanie czy sa pomiedzy monety i ich zamiana
+    for (i = -1; i <= 1; i++) {
+        for (j = -1; j <= 1; j++) {
+            cordYclick = parseInt(idClickedTd.charAt(0));
+            cordXclick = parseInt(idClickedTd.charAt(1));
+            cordX = cordXclick + (j);
+            cordY = cordYclick + (i);
+            cordsXY = '#' + cordX + cordY;
+            // $('#raport').html(cordsXY);
+            if ($(cordsXY).hasClass('bialy') || $(cordsXY).hasClass('czarny')) {
 
-    for(i=-1;i<=1;i++){
-        for(j=-1;j<=1;j++){
-         //  var checkedTd=clickedTd;
+
+                $('#' + idClickedTd).html(imgCoin).addClass(typeClass);
+
+                break;
+            }
         }
     }
 
 
-    $('#raport').html(idClickedTd);
-
-
 }
+$('#startOthelloButton').click(function () {
+    $('.othelloSquare').removeClass('czarny bialy').html('');
+
+
+    $('#44').html('<img src="image/zlotaMoneta.png" class="monetaGry">').addClass('czarny');
+
+    $('#54').html('<img src="image/zlotaMoneta.png" class="monetaGry">').addClass('czarny');
+
+    $('#45').html('<img src="image/srebrnaMoneta.png" class="monetaGry">').addClass('bialy');
+
+    $('#55').html('<img src="image/srebrnaMoneta.png" class="monetaGry">').addClass('bialy');
+
+});
 /*
-rightSquare = checkFirstIdPart + checkSecondIdPart;
-rightSquare = parseFloat(rightSquare);
+ rightSquare = checkFirstIdPart + checkSecondIdPart;
+ rightSquare = parseFloat(rightSquare);
 
-for ( nextSquare = 1; nextSquare > 9; nextSquare++) {
-    rightSquareId = rightSquareId + nextSquare;
-    rightSquareClass = rightSquareId.attr('class');
-    if (!rightSquareClass == classNextTd) {
-        countCoin[nextSquare] = rightSquareId;
-    }
-    else {
-        for (x = 1; countCoin[x].length; x++) {
-            $(countCoin[x]).html('<img src="image/srebrnaMoneta.jpg" class="monetaGry">');
-        }
-    }
-
-}
-    */
+ for ( nextSquare = 1; nextSquare > 9; nextSquare++) {
+ rightSquareId = rightSquareId + nextSquare;
+ rightSquareClass = rightSquareId.attr('class');
+ if (!rightSquareClass == classNextTd) {
+ countCoin[nextSquare] = rightSquareId;
+ }
+ else {
+ for (x = 1; countCoin[x].length; x++) {
+ $(countCoin[x]).html('<img src="image/srebrnaMoneta.jpg" class="monetaGry">');
+ }
+ }
+ cordY=$(this).attr('id').charAt(0);
+ cordX=$(this).attr('id').charAt(1);
+ }
+ */
