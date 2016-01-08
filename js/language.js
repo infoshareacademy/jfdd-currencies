b8d4d3be6fd79a8069108d6272694affb5c70032
emilia <emilia.kamrowska@o2.pl>
2016-01-08 16:41:18 +0100

    funkcja tworzenie ciasteczek

--- js/language.js
+++ js/language.js
@@ -1,5 +1,30 @@
-$(document).ready(function() {
+$(function(){
+function setCookie(cname, cvalue, exdays)   {
+    var d = new Date();
+    d.setTime (d.getTime() +(exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
+}
+$('#jQuerylangEngBtn').click(function() {
+    setCookie('langCookie', 'english', 30);
+});
+
+$('#jQuerylangPlBtn').click(function () {
+    setCookie('langCookie', 'polski', 30);
+});
+
+function getCookie(cname)   {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++)  {
+        var c = ca[i];
+        while (c.charAt(0)==" ") c = c.substring(1);
+        if (c.indexOf(name) ==0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+getCookie('langCookie');
+
+console.log(getCookie('langCookie'));
 
 });
-var english;
-var polski;
