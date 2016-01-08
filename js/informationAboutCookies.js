116cfe30b4b516e95daa35306bb5ffe7ff294558
Jarek M. Siwiński <jmsiwinski@gmail.com>
2016-01-08 16:07:57 +0100

    Merge remote-tracking branch 'remotes/origin/develop' into improvement_footer
    Conflicts:
    	index.html

---
+++ js/informationAboutCookies.js
@@ -0,0 +1,9 @@
+/**
+ * Created by siwexx on 08.01.16.
+ */
+(function() {
+    var container = document.createElement('div'),
+        link = document.createElement('a');
+
+    return true;
+})();
